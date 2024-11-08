f1e947f6add32d8c66986615d57a20a22a8fc82b
Amarhum <muamar171215@gmail.com>
2024-11-08 10:54:45 +0700

    furnitur web template one

--- main.js
+++ main.js
@@ -1,13 +1,72 @@
 import './style.css'
 
-document.querySelector('#site-header').innerHTML = `
-  <div class="navigation-wrapper">
-    <div>Header Area</div>
-  </div>
-`
+// document.querySelector('#site-header').innerHTML = `
+//   <div class="navigation-wrapper">
+//     <div>Header Area</div>
+//   </div>
+// `
 
-document.querySelector('#site-footer').innerHTML = `
-  <div class="footer-wrapper">
-    <div>Footer Area</div>
-  </div>
-`
+// document.querySelector('#site-footer').innerHTML = `
+//   <div class="footer-wrapper">
+//     <div>Footer Area aaaa</div>
+//   </div>
+// `
+
+import AOS from 'aos';
+AOS.init();
+
+
+const hamburger = document.querySelector('#hamburger')
+const mobileMenu = document.querySelector('#mobile-menu')
+const close = document.querySelector('#close')
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.remove('hidden')
+  mobileMenu.classList.toggle('block')
+  console.log('click')
+})
+close.addEventListener('click',()=>{
+  mobileMenu.classList.remove('block')
+  mobileMenu.classList.toggle('hidden')
+})
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 2.5,
+    centerSlides: true,
+
+    
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.custom-next',
+      prevEl: '.custom-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+  const typed = new Typed('#typed-output', {
+    strings: ["We will provide very good quality goods for your dream home and your extended family, products that are guaranteed and made of very strong materials"],
+    typeSpeed: 25,    // Kecepatan pengetikan
+    backSpeed: 25,    // Kecepatan penghapusan
+    loop: true        // Animasi akan terus berulang
+  });
+  const typedFooter = new Typed('#typed-output-footer', {
+    strings: ["Lörem ipsum farade kroresade planat. Semipons ner,deciliga men niponade. Lörem ipsum Lörem ipsum farade kroresade planat. Semipons ner, deciliga men niponade. Lörem ipsum "],
+    typeSpeed: 25,    // Kecepatan pengetikan
+    backSpeed: 25,    // Kecepatan penghapusan
+    loop: true        // Animasi akan terus berulang
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ export default {
     "./*.{html,js}",
     "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,  // Center container by default
+      padding: {
+        DEFAULT: '1rem', // Padding default
+        sm: '1.5rem',  // Padding for small screens
+        md: '2rem',    // Padding for medium screens
+        lg: '2.5rem',  // Padding for large screens
+        xl: '3rem',    // Padding for extra large screens
+      },
+    },
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+      },
+      colors: {
+        primary: '#152D32',
+        secondary: '#FFC100',
+        paragraf : '#A1ABAD',
+        balckText : '#091416',
+        footer : '#081315',
+      },
+
+    },
   },
   plugins: [],
 }
