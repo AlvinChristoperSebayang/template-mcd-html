cdca7eb2c64a246024df411ca54d316705b929fc
satriajun <satria.junmulyana_ti21@nusaputra.ac.id>
2024-11-19 09:18:44 +0700

    furniture

--- main.js
+++ main.js
@@ -1,14 +1,61 @@
 import './style.css'
 
 
-document.querySelector('#site-header').innerHTML = `
-  <div class="navigation-wrapper">
-    <div>Header Area</div>
-  </div>
-`
+// document.querySelector('#site-header').innerHTML = `
+//   <div class="navigation-wrapper">
+//     <div>Header Area</div>
+//   </div>
+// `
 
-document.querySelector('#site-footer').innerHTML = `
-  <div class="footer-wrapper">
-    <div>Footer Area</div>
-  </div>
-`
+// document.querySelector('#site-footer').innerHTML = `
+//   <div class="footer-wrapper">
+//     <div>Footer Area</div>
+//   </div>
+// `
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 2,
+//   spaceBetween: 20,
+
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.custom-next',
+    prevEl: '.custom-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
