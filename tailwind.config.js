a6a027d75febe5ce7c07fbce3759f9d1bf9cc691
satriajun <satria.junmulyana_ti21@nusaputra.ac.id>
2024-12-27 16:09:40 +0700

    agency

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,30 @@ export default {
   content: [
     "./*.{html,js}",
     "./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+    theme: {
+      screens: {
+        'sm': '375px',    // Extra small devices (phones)
+        'md': '768px',    // Medium devices (tablet)
+        'lg': '1024px',   // Large devices (small desktops)
+        'xl': '1280px',   // Extra large devices (large desktops)
+        'xl2': '1440px',  // 2x Extra large devices (larger screens)
+      },
+      container: {
+        center: true,  // Center container by default
+        padding: {
+          DEFAULT: '1rem', // Padding default
+          sm: '1.5rem',  // Padding for small screens
+          md: '2rem',    // Padding for medium screens
+          lg: '40px',  // Padding for large screens
+          xl: '60px',    // Padding for extra large screens
+          xl2: '120px',    // Padding for extra large screens
+        },
+      },
+    extend: {
+      fontFamily: {
+        satoshi: ["satoshi" , "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
-
