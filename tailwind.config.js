a28e348d8d0967e825054bd575787efa20829cc6
Warda Amalia <amaliawarda411@gmail.com>
2024-12-30 20:37:59 +0700

    mengupdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./*.{html,js}",
-    "./src/**/*.{html,js}"],
+    "./src//*.{html,js}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'helvetica-neue': ['Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+      colors: { primary: '#7240FF', },
+      fontSize: {
+        'responsive-h2': ['64px', {
+          sm: '32px',
+          md: '40px',
+          lg: '48px',
+        }],
+      },
+      container: {
+        center: true,
+        padding: '0rem', 
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1200px',
+        },
+      }      
+    },
   },
   plugins: [],
-}
 
+}
