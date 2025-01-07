3db618881b9ede0e652ed02505ea9bff8053b182
AlvinChristoperSebayang <csa7045@gmail.com>
2025-01-07 17:21:15 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,24 @@ export default {
     "./*.{html,js}",
     "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        breakpoint: {
+          mobile: '375px',
+          tablet: '768px',
+          laptop: '1024px',
+          desktop: '1440px'
+        }
+      },
+    },
     screens: {
       'mobile': '375px',
+      'tablet': '768px',
       'laptop': '1024px',
       'desktop': '1440px'
     }
