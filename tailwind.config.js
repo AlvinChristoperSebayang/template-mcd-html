5b37ff9479c62c5a092deb8c678536b6d061a33f
Jamaludin ahmad <jamaludinahmad013@gmail.com>
2025-01-13 11:24:25 +0700

    add: footer dan mobile navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,34 @@ export default {
       fontFamily: {
         'inter': ['Inter', 'sans-serif']
       },
+      fontSize: {
+        xl: ['64px', '88px'],
+        lg: ['48px', '64px'],
+        md: ['40px', '54px'],
+        sm: ['32px', '42px'],
+        xs: ['24px', '32px'],
+      },
       center: true,
       container: {
-        'mobile': '375px',
-        'laptop': '1024px',
-        'tablet': '768px',
-        'desktop': '1440px'  
+        padding:{
+          mobile: '24px',
+          laptop: '40px',
+          desktop: '60px',
+        },
+        breakpoints: {
+          'mobile': '375px',
+          'laptop': '1024px',
+          'tablet': '768px',
+          'desktop': '1440px'  
+        }
+
       }
     },
     screens: {
       'mobile': '375px',
       'laptop': '1024px',
       'desktop': '1440px'
-    }
+    },
   },
   plugins: [],
 }
