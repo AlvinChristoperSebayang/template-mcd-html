065883f8f1e0568efb61f92d72fcec26bd746736
YudaAnanda245 <0803yudha@gmail.com>
2025-02-28 02:26:08 +0700

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-    "./src/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     screens: {
-      'sm': '375px',     
-      'md': '768px',     
-      'lg': '1024px',    
-      'xl': '1360px',    
-      'xl2': '1440px',   
+      default: "472px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1360px",
+      xl2: "1440px",
     },
     container: {
-      center: true,   
+      center: true,
       padding: {
-        DEFAULT: '1rem',  
-        sm: '1.25rem',   
-        md: '2rem',     
-        lg: '40px',  
-        xl: '60px',     
-        xl2: '120px',    
+        DEFAULT: "1rem",
+        sm: "1.25rem",
+        md: "2rem",
+        lg: "40px",
+        xl: "60px",
+        xl2: "120px",
       },
     },
 
-    extend: {
-},
+    extend: {},
   },
   plugins: [],
-}
+};
 
