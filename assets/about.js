972124acb17142a9ff2cb72f0a8d99005fc8ec23
GilangSatriaTekkom <tekkomgilang26@gmail.com>
2025-03-17 16:17:26 +0700

    17/03/2025 - Addjust web animation

--- assets/about.js
+++ assets/about.js
@@ -1,189 +1,534 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { preloadTL } from '../main.js';
+import { preloadTL } from "../main.js";
 
 gsap.registerPlugin(ScrollTrigger);
 let isMobile = window.innerWidth < 768;
 
-const path = document.querySelector('#animatedSvg path');
-const percentage = document.getElementById('percentage');
+const path = document.querySelector("#animatedSvg path");
+const percentage = document.getElementById("percentage");
 const pathLength = path.getTotalLength();
 path.style.strokeDasharray = pathLength;
 path.style.strokeDashoffset = pathLength;
 
-let heroSection = gsap.timeline({paused:true});
-let titleHero = document.querySelector('.titleHero');
+let heroSection = gsap.timeline({ paused: true });
+let titleHero = document.querySelector(".titleHero");
 
-if(titleHero){
+if (titleHero) {
   let splitText = new SplitType(titleHero, {
-    types: 'lines',
+    types: "lines",
   });
 }
 
-let lines = document.querySelectorAll('.titleHero .line');
-lines.forEach(line => {
-  let wrapper = document.createElement('div');
-  wrapper.classList.add('line-wrapper');
+let lines = document.querySelectorAll(".titleHero .line");
+lines.forEach((line) => {
+  let wrapper = document.createElement("div");
+  wrapper.classList.add("line-wrapper");
   line.parentNode.insertBefore(wrapper, line);
   wrapper.appendChild(line);
 });
 
 lines.forEach((line, index) => {
-  heroSection.fromTo(line, {
-    yPercent: 300,
-    rotation: -15,
-    transformOrigin: '50% 50%',
-  }, {
-    yPercent: 0,
-    rotation: 0,
-    duration: 0.8,
-    ease: 'cubic-bezier(0.36, 0, 0.66, -0.56)',
-  },'<.2');
+  heroSection.fromTo(
+    line,
+    {
+      yPercent: 300,
+      rotation: -15,
+      transformOrigin: "50% 50%",
+    },
+    {
+      yPercent: 0,
+      rotation: 0,
+      duration: 0.8,
+      ease: "cubic-bezier(0.36, 0, 0.66, -0.56)",
+    },
+    "<.2"
+  );
 });
 
-let descHero = document.querySelector('.description-hero');
-if(descHero){
+let descHero = document.querySelector(".description-hero");
+if (descHero) {
   let splitText = new SplitType(descHero, {
-    types: 'lines',
+    types: "lines",
   });
 }
-let descLines = document.querySelectorAll('.description-hero .line');
-descLines.forEach(lineDesc => {
-  let wrapperdesc = document.createElement('div');
-  wrapperdesc.classList.add('line-wrapper');
+let descLines = document.querySelectorAll(".description-hero .line");
+descLines.forEach((lineDesc) => {
+  let wrapperdesc = document.createElement("div");
+  wrapperdesc.classList.add("line-wrapper");
   lineDesc.parentNode.insertBefore(wrapperdesc, lineDesc);
   wrapperdesc.appendChild(lineDesc);
 });
 
 descLines.forEach((line, index) => {
-  heroSection.fromTo(line, {
-    yPercent: 300,
-    rotation: -15,
-    transformOrigin: '50% 50%',
-  }, {
-    yPercent: 0,
-    rotation: 0,
-    duration: 0.8,
-    ease: 'cubic-bezier(0.36, 0, 0.66, -0.56)',
-  },'<.2');
+  heroSection.fromTo(
+    line,
+    {
+      yPercent: 300,
+      rotation: -15,
+      transformOrigin: "50% 50%",
+    },
+    {
+      yPercent: 0,
+      rotation: 0,
+      duration: 0.8,
+      ease: "cubic-bezier(0.36, 0, 0.66, -0.56)",
+    },
+    "<.2"
+  );
 });
-heroSection.to('.patternHero',{
-  scale:1,
-  opacity:1,
-  duration:0.5,
-  ease:'power3.inOut'
-},'<')
-heroSection.to('.ornamentLogoSpin',{
-  top:'5rem',
-  opacity:1,
-  duration:1,
-  ease:'power3.inOut'
-},'<.3')
-heroSection.to('.ornamentPlusTitle',{
-  bottom:'-32px',
-  opacity:1,
-  duration:1,
-  ease:'power3.inOut'
-},'<')  
-heroSection.to('.phoneOrnament--wrapper',{
-  opacity:1,
-  duration:0.5,
-  marginTop:'50px',
-},'<.3')
-heroSection.to('.bar_phone--wrapper',{
-  bottom:'0px',
-  opacity:1,
-  duration:0.5,
-  ease:'power3.inOut'
-},'>')
+heroSection.to(
+  ".patternHero",
+  {
+    scale: 1,
+    opacity: 1,
+    duration: 0.5,
+    ease: "power3.inOut",
+  },
+  "<"
+);
+heroSection.to(
+  ".ornamentLogoSpin",
+  {
+    top: "5rem",
+    opacity: 1,
+    duration: 1,
+    ease: "power3.inOut",
+  },
+  "<.3"
+);
+heroSection.to(
+  ".ornamentPlusTitle",
+  {
+    bottom: "-32px",
+    opacity: 1,
+    duration: 1,
+    ease: "power3.inOut",
+  },
+  "<"
+);
+heroSection.to(
+  ".phoneOrnament--wrapper",
+  {
+    opacity: 1,
+    duration: 0.5,
+    marginTop: "50px",
+  },
+  "<.3"
+);
+heroSection.to(
+  ".bar_phone--wrapper",
+  {
+    bottom: "0px",
+    opacity: 1,
+    duration: 0.5,
+    ease: "power3.inOut",
+  },
+  ">"
+);
 // heroSection.to('.contentPhone',{
 //   marginTop:'-350px',
 //   duration:3,
 //   ease:'power1.inOut'
 // },'<.3')
-window.onload = function () {
-  preloadTL.eventCallback("onComplete", function() {
-    heroSection.play();
-  });
 
-};
 let doctorTl1 = gsap.timeline({
   scrollTrigger: {
     trigger: ".section-introducing",
     start: "top center",
     end: "20% center",
     // markers: true
-  }
-
-
+  },
 });
-doctorTl1.to('.image-logoOrnament', {
+doctorTl1.to(".image-logoOrnament", {
   opacity: 1,
-  duration: 2
-})
+  duration: 2,
+});
 
 let doctorTl2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".section-introducing",
-    start: `${isMobile ? '10%' : '22%'} center`,
+    start: `${isMobile ? "10%" : "22%"} center`,
     end: "40% center",
     // markers: true
-  }
+  },
 });
 
-doctorTl2.to('.image-logoOrnament', {
+doctorTl2.to(".image-logoOrnament", {
   rotation: 180,
   transformOrigin: "center center",
   duration: 1,
   width: 48,
   height: 48,
 });
-doctorTl2.fromTo('.titleAnimate', {
-  opacity: 0,
-  duration: 0.5,
-  y: '100px'
-}, {
-  opacity: 1,
-  duration: 0.5,
-  y: '0px',
-  stagger: .2
-},'>.1')
-
-if (!isMobile) {
-  doctorTl2.fromTo('.doctorCard', {
+doctorTl2.fromTo(
+  ".titleAnimate",
+  {
     opacity: 0,
     duration: 0.5,
-    y: '100px',
-
-  }, {
+    y: "100px",
+  },
+  {
     opacity: 1,
     duration: 0.5,
-    y: '0px',
-    stagger: 0.2
-  },'>.1');
-} else {
-  let cardDoctors = document.querySelectorAll('.doctorCard');
-  cardDoctors.forEach((card, index) => {
-    gsap.fromTo(card, {
+    y: "0px",
+    stagger: 0.2,
+  },
+  ">.1"
+);
+
+if (!isMobile) {
+  doctorTl2.fromTo(
+    ".doctorCard",
+    {
       opacity: 0,
-      y: '100px'
-    }, {
-      scrollTrigger: {
-        trigger: card,
-        start: 'top 50%',
-        // markers:true
-      },
+      duration: 0.5,
+      y: "100px",
+    },
+    {
       opacity: 1,
-      y: '0px',
-      duration: 0.5
-    });
+      duration: 0.5,
+      y: "0px",
+      stagger: 0.2,
+    },
+    ">.1"
+  );
+} else {
+  let cardDoctors = document.querySelectorAll(".doctorCard");
+  cardDoctors.forEach((card, index) => {
+    gsap.fromTo(
+      card,
+      {
+        opacity: 0,
+        y: "100px",
+      },
+      {
+        scrollTrigger: {
+          trigger: card,
+          start: "top 50%",
+          // markers:true
+        },
+        opacity: 1,
+        y: "0px",
+        duration: 0.5,
+      }
+    );
   });
- 
 }
-let hamburgerButton = document.querySelector('.hamburger-bars');
-let mobileNav = document.querySelector('.mobileNav');
-hamburgerButton.addEventListener('click', function () {
-  hamburgerButton.classList.toggle('active');
-  mobileNav.classList.toggle('active');
-  console.log('active')
-})
+let hamburgerButton = document.querySelector(".hamburger-bars");
+let mobileNav = document.querySelector(".mobileNav");
+hamburgerButton.addEventListener("click", function () {
+  hamburgerButton.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  console.log("active");
+});
+
+function animateCounter(
+  target,
+  endValue,
+  duration,
+  triggerElement = ".sectionCounter",
+  endText = ""
+) {
+  let element = document.getElementById(target);
+
+  if (!element) {
+    console.error(`Element with ID "${target}" not found.`);
+    return;
+  }
+
+  let counterTL = gsap.fromTo(
+    element,
+    { textContent: 0 },
+    {
+      textContent: endValue,
+      duration: duration,
+      ease: "power1.inOut",
+      snap: { textContent: 1 },
+      onUpdate: function () {
+        element.innerText = Math.round(this.targets()[0].textContent) + endText;
+      },
+      scrollTrigger: {
+        trigger: triggerElement,
+        start: "top center",
+        end: "20% center",
+      },
+    }
+  );
+
+  return counterTL;
+}
+
+animateCounter("counter", 300, 4, ".sectionCounter", "+");
+animateCounter(
+  "counterPartner",
+  100,
+  4,
+  isMobile ? ".title-section" : ".sectionCounter",
+  ""
+);
+animateCounter(
+  "counterDoctor",
+  50,
+  4,
+  isMobile ? ".title-section" : ".sectionCounter",
+  "+"
+);
+animateCounter(
+  "counterReview",
+  100,
+  4,
+  isMobile ? ".title-section" : ".sectionCounter",
+  "k"
+);
+
+window.onload = function () {
+  preloadTL.eventCallback("onComplete", function () {
+    heroSection.play();
+  });
+};
+
+let sectionAppHealth = document.querySelectorAll(
+  ".sectionCounter .title-section "
+);
+let sectionWhyHealth = document.querySelectorAll(
+  ".sectionWhyHealth .title-section "
+);
+
+sectionAppHealth.forEach((title) => {
+  let splitText = new SplitType(title, { types: "lines" });
+
+  splitText.lines.forEach((line) => {
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("line-wrapper");
+
+    line.parentNode.insertBefore(wrapper, line);
+    wrapper.appendChild(line);
+  });
+
+  let titleSectionsLine = title.querySelectorAll(".line");
+  let isMobile = window.innerWidth < 768;
+
+  gsap.fromTo(
+    titleSectionsLine,
+    {
+      yPercent: 300,
+      rotation: -15,
+      transformOrigin: "50% 50%",
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      rotation: 0,
+      opacity: 1,
+      duration: 0.8,
+      ease: "cubic-bezier(0.36, 0, 0.66, -0.56)",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: isMobile ? ".title-section" : ".sectionCounter",
+        start: "top center",
+        end: "20% center",
+      },
+    }
+  );
+});
+
+sectionWhyHealth.forEach((title) => {
+  let splitText = new SplitType(title, { types: "lines" });
+
+  splitText.lines.forEach((line) => {
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("line-wrapper");
+
+    line.parentNode.insertBefore(wrapper, line);
+    wrapper.appendChild(line);
+  });
+
+  let titleSectionsLine = title.querySelectorAll(".line-wrapper .line");
+  let isMobile = window.innerWidth < 768;
+  let buttonSectionWhyHealth = document.querySelector(".buttonWhyHealth");
+
+  gsap.fromTo(
+    [titleSectionsLine, buttonSectionWhyHealth],
+    {
+      yPercent: -300,
+      transformOrigin: "50% 50%",
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      rotation: 0,
+      opacity: 1,
+      duration: 1,
+      ease: "cubic-bezier(0.36, 0, 0.66, -0.56)",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".sectionWhyHealth",
+        start: "top center",
+        end: "20% center",
+      },
+    }
+  );
+});
+
+gsap.fromTo(
+  ".textContent-wrap *",
+  {
+    xPercent: -300,
+    opacity: 0,
+  },
+  {
+    xPercent: 0,
+    opacity: 1,
+    duration: 0.8,
+    delay: 0.4,
+    ease: "power3.Out",
+    scrollTrigger: {
+      trigger: ".sectionCounter",
+      start: "top center",
+      end: "20% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".counterPartner-item",
+  {
+    xPercent: 300,
+    opacity: 0,
+  },
+  {
+    xPercent: 0,
+    opacity: 1,
+    duration: 0.5,
+    delay: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: isMobile ? ".title-section" : ".sectionCounter",
+      start: "top center",
+      end: "20% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".counterDoctor-item",
+  {
+    xPercent: 300,
+    opacity: 0,
+  },
+  {
+    xPercent: 0,
+    opacity: 1,
+    duration: 0.5,
+    delay: 1.3,
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: isMobile ? ".title-section" : ".sectionCounter",
+      start: "top center",
+      end: "20% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".counterReview-item",
+  {
+    xPercent: 300,
+    opacity: 0,
+  },
+  {
+    xPercent: 0,
+    opacity: 1,
+    duration: 0.5,
+    delay: 1.6,
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: isMobile ? ".title-section" : ".sectionCounter",
+      start: "top center",
+      end: "20% center",
+    },
+  }
+);
+
+let subSection = document.querySelectorAll("#subsection-title");
+
+subSection.forEach((sub) => {
+  let splitText = new SplitType(sub, { types: "lines" });
+
+  splitText.lines.forEach((line) => {
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("line-wrapper");
+
+    line.parentNode.insertBefore(wrapper, line);
+    wrapper.appendChild(line);
+  });
+
+  let subSectionLine = sub.querySelectorAll(".line");
+
+  gsap.fromTo(
+    subSectionLine,
+    {
+      yPercent: 300,
+      rotation: -15,
+      transformOrigin: "50% 50%",
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      rotation: 0,
+      opacity: 1,
+      duration: 0.8,
+      ease: "cubic-bezier(0.36, 0, 0.66, -0.56)",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".sectionCounter",
+        start: "top center",
+        end: "20% center",
+      },
+    }
+  );
+});
+
+const subSectionParagraph = document.querySelectorAll("#subsection-paragraph");
+const split = new SplitType(subSectionParagraph, { types: "chars" });
+
+gsap.from(split.chars, {
+  x: 150,
+  opacity: 0,
+  duration: 0.7,
+  ease: "power4",
+  stagger: 0.04,
+  scrollTrigger: {
+    trigger: ".sectionCounter",
+    start: "top center",
+    end: "20% center",
+  },
+});
+
+const itemShape = document.querySelectorAll("#item-shape");
+
+gsap.from(itemShape, {
+  rotation: 45,
+  scale: 0,
+  duration: 0.7,
+  ease: "power4",
+  scrollTrigger: {
+    trigger: ".sectionCounter",
+    start: "top center",
+    end: "20% center",
+  },
+});
+
+const appHealthWrap = document.querySelectorAll("#application-health-wrap");
+
+gsap.from(appHealthWrap, {
+  opacity: 0,
+  duration: 1,
+  ease: "power4",
+  scrollTrigger: {
+    trigger: ".sectionCounter",
+    start: "top center",
+    end: "20% center",
+  },
+});
--- assets/blog.js
+++ assets/blog.js
@@ -1,31 +1,74 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { preloadTL } from "../main.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const animateAboutHeader = () => {
-  const blogLabel = document.querySelector(".aboutHeader .label");
-  const blogTitle = document.querySelector(".aboutHeader h1");
-  const blogText = document.querySelector(".aboutHeader p");
+const animateBlogHeader = () => {
+  const blogLabel = document.querySelector(".blogHeader label");
+  const blogTitle = document.querySelector(".blogHeader h1");
+  const blogText = document.querySelector(".blogHeader p");
 
-  gsap.set([blogLabel, blogTitle, blogText], { opacity: 0, y: 50 });
+  let titleHero = document.querySelector(".blogHeader .titleHero");
+
+  let lines = [];
+  if (titleHero) {
+    new SplitType(titleHero, { types: "lines" });
+    lines = document.querySelectorAll(".titleHero .line");
+
+    lines.forEach((lineDesc) => {
+      let wrapperdesc = document.createElement("div");
+      wrapperdesc.classList.add("line-wrapper");
+      lineDesc.parentNode.insertBefore(wrapperdesc, lineDesc);
+      wrapperdesc.appendChild(lineDesc);
+    });
+  }
+
+  let lineWrapper = document.querySelectorAll(".titleHero .line");
+
+  gsap.set([blogLabel, blogText], {
+    opacity: 0,
+    y: 100,
+    visibility: "visible",
+  });
 
   ScrollTrigger.create({
     trigger: blogLabel,
     start: "top 80%",
     onEnter: () => {
       const tl = gsap.timeline();
-      tl.to(blogLabel, { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" })
-        .to(
-          blogTitle,
-          { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" },
-          "+=0.3"
-        )
-        .to(
-          blogText,
-          { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" },
-          "-=0.3"
+
+      tl.to(blogLabel, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: "power2.out",
+      });
+      tl.to(blogTitle, { opacity: 1, ease: "power2.out" }, "-=0.3");
+      lineWrapper.forEach((line, index) => {
+        tl.fromTo(
+          line,
+          {
+            opacity: 0,
+            yPercent: 300,
+            rotation: -15,
+            transformOrigin: "50% 50%",
+          },
+          {
+            opacity: 1,
+            yPercent: 0,
+            rotation: 0,
+            duration: 0.8,
+            ease: "cubic-bezier(0.36, 0, 0.66, -0.56)",
+          },
+          `<${index * 0.2}`
         );
+      });
+      tl.to(
+        blogText,
+        { opacity: 1, y: 0, duration: 0.8, ease: "power2.out" },
+        "-=0.3"
+      );
     },
     once: true,
   });
@@ -33,34 +76,33 @@ const animateAboutHeader = () => {
 
 const animateAboutFirstListBlog = () => {
   const firstListBlog = document.querySelector(".firstListBlog");
-  const firstBlogThumbnail = firstListBlog.querySelector(".thumbnailFirstBlog");
-  const firstBlog = firstListBlog.querySelector(".contentFirstBlog");
+  // const firstBlogThumbnail = firstListBlog.querySelector(".thumbnailFirstBlog");
+  // const firstBlog = firstListBlog.querySelector(".contentFirstBlog");
   const blogList = firstListBlog.querySelector(".blogList");
   const blogListItems = blogList.querySelectorAll(".blogList .card");
+  gsap.set([...blogListItems], { opacity: 0, y: 50 });
+  // gsap.set(firstBlogThumbnail, { opacity: 0, scale: 1.2 });
 
-  gsap.set([firstBlog, ...blogListItems], { opacity: 0, y: 50 });
-  gsap.set(firstBlogThumbnail, { opacity: 0, scale: 1.2 });
-
-  ScrollTrigger.create({
-    trigger: firstListBlog,
-    start: "top 80%",
-    onEnter: () => {
-      const tl = gsap.timeline();
-      tl.to(firstBlog, {
-        opacity: 1,
-        y: 0,
-        duration: 0.5,
-        ease: "power2.out",
-        delay: 0.3,
-      }).to(
-        firstBlogThumbnail,
-        { opacity: 1, scale: 1, duration: 0.5, ease: "power2.out" },
-        "-=0.3"
-      );
-    },
-    once: true,
-  });
-
+  // ScrollTrigger.create({
+  //   trigger: firstListBlog,
+  //   start: "top 80%",
+  //   onEnter: () => {
+  //     const tl = gsap.timeline();
+  //     tl.to(firstBlog, {
+  //       opacity: 1,
+  //       y: 0,
+  //       duration: 0.5,
+  //       ease: "power2.out",
+  //       delay: 0.3,
+  //     }).to(
+  //       firstBlogThumbnail,
+  //       { opacity: 1, scale: 1, duration: 0.5, ease: "power2.out" },
+  //       "-=0.3"
+  //     );
+  //   },
+  //   once: true,
+  // });
+  const isMobile = window.innerWidth <= 768;
   ScrollTrigger.create({
     trigger: blogListItems,
     start: "top 80%",
@@ -69,9 +111,9 @@ const animateAboutFirstListBlog = () => {
       tl.to(blogListItems, {
         opacity: 1,
         y: 0,
-        duration: 0.5,
+        duration: 0.8,
         ease: "power2.out",
-        stagger: 0.2,
+        stagger: 0.4,
       });
     },
   });
@@ -92,9 +134,9 @@ const animateListPopularBlog = () => {
       tl.to(listBLog, {
         opacity: 1,
         y: 0,
-        duration: 0.5,
+        duration: 0.8,
         ease: "power2.out",
-        stagger: 0.2,
+        stagger: 0.4,
       });
     },
   });
@@ -107,11 +149,11 @@ const animateListPopularBlog = () => {
       tl.to(popularTitle, {
         opacity: 1,
         y: 0,
-        duration: 0.5,
+        duration: 0.8,
         ease: "power2.out",
       }).to(
         popularText,
-        { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" },
+        { opacity: 1, y: 0, duration: 0.8, ease: "power2.out" },
         "-=0.3"
       );
     },
@@ -123,19 +165,147 @@ const handleResize = () => {
   const isMobile = window.innerWidth <= 768;
 
   if (isMobile) {
-    gsap.set(".aboutHeader .label, .aboutHeader h1, .aboutHeader p", { y: 30 });
+    gsap.set(".blogHeader .label, .blogHeader h1, .blogHeader p", { y: 30 });
     gsap.set(".firstListBlog .contentFirstBlog, .blogList .card", { y: 30 });
     gsap.set(".popularBlog h2, .popularBlog p, .popularBlog .card", { y: 30 });
   } else {
-    gsap.set(".aboutHeader .label, .aboutHeader h1, .aboutHeader p", { y: 50 });
+    gsap.set(".blogHeader .label, .blogHeader h1, .blogHeader p", { y: 50 });
     gsap.set(".firstListBlog .contentFirstBlog, .blogList .card", { y: 50 });
     gsap.set(".popularBlog h2, .popularBlog p, .popularBlog .card", { y: 50 });
   }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  animateAboutHeader();
   animateAboutFirstListBlog();
   animateListPopularBlog();
   window.addEventListener("resize", handleResize);
+  window.onload = function () {
+    gsap.set(".blogHeader label, .blogHeader h1, .blogHeader p", {
+      opacity: 0,
+    });
+
+    preloadTL.eventCallback("onComplete", function () {
+      animateBlogHeader();
+    });
+  };
 });
+
+let titleSections = document.querySelectorAll(".title-section");
+let sectionParagraph = document.querySelectorAll(".sectionParagraph");
+
+titleSections.forEach((line) => {
+  let splitText = new SplitType(line, { types: "lines" });
+
+  splitText.lines.forEach((line) => {
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("line-wrapper");
+
+    line.parentNode.insertBefore(wrapper, line);
+    wrapper.appendChild(line);
+  });
+});
+
+sectionParagraph.forEach((paragraph) => {
+  let splitText = new SplitType(paragraph, { types: "lines" });
+
+  splitText.lines.forEach((line) => {
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("line-wrapper");
+
+    line.parentNode.insertBefore(wrapper, line);
+    wrapper.appendChild(line);
+  });
+
+  const pararaphLines = document.querySelectorAll(".line-wrapper");
+  const learnButton = document.querySelectorAll(".learnButton");
+  const isMobile = window.innerWidth <= 768;
+
+  gsap.fromTo(
+    [pararaphLines, learnButton],
+    {
+      yPercent: 300,
+      transformOrigin: "50% 50%",
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      opacity: 1,
+      duration: 1.5,
+      ease: "power2.out",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".sectionHealth",
+        start: "top center",
+        end: "20% center",
+      },
+    }
+  );
+});
+let titleSectionLines = document.querySelectorAll(".line-wrapper");
+
+titleSections.forEach((line, index) => {
+  gsap.fromTo(
+    titleSectionLines,
+    {
+      yPercent: 300,
+      transformOrigin: "50% 50%",
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      opacity: 1,
+      duration: 1.5,
+      ease: "power2.out",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".sectionHealth",
+        start: "top center",
+        end: "20% center",
+      },
+    }
+  );
+});
+
+let imageWrap = document.querySelectorAll(".imageWrap");
+gsap.fromTo(
+  imageWrap,
+  {
+    xPercent: 300,
+    transformOrigin: "50% 50%",
+    opacity: 0,
+  },
+  {
+    xPercent: 0,
+    opacity: 1,
+    duration: 1.5,
+    ease: "power2.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".sectionHealth",
+      start: "top center",
+      end: "20% center",
+    },
+  }
+);
+
+let dateTitleLabel = document.querySelectorAll(".dateLabel.titleLabel");
+gsap.fromTo(
+  dateTitleLabel,
+  {
+    xPercent: -300,
+    transformOrigin: "50% 50%",
+    opacity: 0,
+  },
+  {
+    xPercent: 0,
+    opacity: 1,
+    duration: 1.5,
+    ease: "power2.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".sectionHealth",
+      start: "top center",
+      end: "20% center",
+    },
+  }
+);
