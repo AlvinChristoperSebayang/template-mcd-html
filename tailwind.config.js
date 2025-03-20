a5a78b6b1e136c084459b2cc5ee696c2aa3a3293
YudaAnanda245 <0803yudha@gmail.com>
2025-03-20 12:48:13 +0700

    wedding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-    "./src/**/*.{html,js}"],
-    theme: {
-      fontFamily: {
-        body:['Darker Grotesque', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
-        default: ['Darker Grotesque', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        darkerGrotesque: ['Darker Grotesque', 'sans-serif'],
-        roboto: ['Roboto', 'sans-serif'],
-        PlusJakartaSans: ['Plus Jakarta Sans', 'Poppins', 'sans-serif'],
-        firaCode: ['Fira Code', 'monospace'],  
-        bricolage : ['Bricolage Grotesque', 'sans-serif'],
-        PlayfairDisplay: ['Playfair Display', 'serif'], 
-        Ephesis:  ['Ephesis', 'cursive'],
-        DancingScript: ['Dancing Script', 'serif'], 
-        TimesNewRoman: ['TimesNewRoman', 'serif'],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      body: ["Darker Grotesque", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+      default: ["Darker Grotesque", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      darkerGrotesque: ["Darker Grotesque", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      PlusJakartaSans: ["Plus Jakarta Sans", "Poppins", "sans-serif"],
+      firaCode: ["Fira Code", "monospace"],
+      bricolage: ["Bricolage Grotesque", "sans-serif"],
+      PlayfairDisplay: ["Playfair Display", "serif"],
+      Ephesis: ["Ephesis", "cursive"],
+      DancingScript: ["Dancing Script", "serif"],
+      TimesNewRoman: ["TimesNewRoman", "serif"],
     },
-  
-      extend: {},
+    extend: {
+      colors: {
+        primary: "#4E4E50",
+      },
     },
+  },
   plugins: [],
-}
-
+};
