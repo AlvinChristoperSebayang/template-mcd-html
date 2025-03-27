d0a997badac3c30635cfb3a7c0148ccafc14c92a
GilangSatriaTekkom <tekkomgilang26@gmail.com>
2025-03-27 13:03:22 +0700

    17/03/2025 - Revisi Web

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,39 @@
 export default {
-  content: [
-    "./*.{html,js}",
-    "./src//*.{html,js}"
-  ],
+  content: ["./*.{html,js}", "./src//*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        'helvetica-neue': ['Helvetica Neue', 'Arial', 'sans-serif'],
+      backgroundImage: {
+        footer: "url('/background_footer.png')",
+        footerMobile: "url('/mobileFooterBg.png')",
+        footerLight: "url('/footerLight.png')",
+        header: "url('/desktopHeadBg.png')",
+        mobileHeader: "url('/mobileHeadBg.png')",
       },
-      colors: { primary: '#7240FF', },
+      fontFamily: {
+        "helvetica-neue": ["Helvetica Neue", "Arial", "sans-serif"],
+      },
+      colors: { primary: "#7240FF" },
       fontSize: {
-        'responsive-h2': ['64px', {
-          sm: '32px',
-          md: '40px',
-          lg: '48px',
-        }],
+        "responsive-h2": [
+          "64px",
+          {
+            sm: "32px",
+            md: "40px",
+            lg: "48px",
+          },
+        ],
       },
       container: {
         center: true,
-        padding: '0rem', 
+        padding: "0rem",
         screens: {
-          sm: '640px',
-          md: '768px',
-          lg: '1024px',
-          xl: '1200px',
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1200px",
         },
-      }      
+      },
     },
   },
   plugins: [],
-
-}
+};
