4b77863ddc8a4b0bbb835c178a48515d59b062f0
GilangSatriaTekkom <tekkomgilang26@gmail.com>
2025-06-02 15:48:31 +0700

    02/06/2025 - revisi dan perbaikan responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-    "./src/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
-        '1360': '1360px',
+        1360: "1360px",
       },
       fontFamily: {
-        'instrument': ['"Instrument Sans"'],
-        'plus-jakarta': ['"Plus Jakarta Sans"'], 
+        instrument: ['"Instrument Sans"'],
+        "plus-jakarta": ['"Plus Jakarta Sans"'],
+        playfair: ["Playfair"],
       },
       rotate: {
-        '-180': '-180deg',
-      }
+        "-180": "-180deg",
+      },
+      colors: {
+        primary: "#a0c093",
+        secondary: "#FBBF24",
+        customBlack: "#161111",
+      },
     },
   },
   plugins: [],
-}
-
+};
